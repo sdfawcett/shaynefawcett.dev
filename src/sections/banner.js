342a6c3d00e73bdb5281fc233e89dac718d8e5f8
Shayne <shayne.fawcett@gmail.com>
2022-05-24 15:54:42 -0400

    added responsive bg svg pattern to hero and header

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -1,108 +1,156 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import { Box, Container, Image, Heading, Text } from 'theme-ui';
 import { keyframes } from '@emotion/core';
 import gravatarImage from 'assets/gravatar-image.png';
 import bannerIcon5 from 'assets/banner-icon-1-5.svg';
 import bannerIcon6 from 'assets/banner-icon-1-6.svg';
 import bannerIcon7 from 'assets/dot-pattern.svg';
-import macbookPage from 'assets/macbook-mockup-webpage.png';
+import macbookPage from 'assets/macbook-mockup-webpage.webp';
 import { motion } from 'framer-motion';
 import Typed from 'react-typed';
 
 const Banner = () => {
+
+  const [mobile, setMobile] = useState(undefined)
+
+  useEffect(() => {
+    const updateMobile = () => {
+      setMobile(window.innerWidth < 700 ? true : false)
+    }
+
+    updateMobile()
+    window.addEventListener('resize', updateMobile)
+    return () => {
+      window.removeEventListener('resize', updateMobile)
+    }
+  }, [])
+
   return (
     <Box as="section" id="banner" sx={styles.banner}>
-      <Container sx={styles.container}>
+      <Box sx={styles.bg}>
+        <Container sx={styles.container}>
 
-        <Image
-          sx={styles.bannerIcon5}
-          className="bannerIcon"
-          alt="banner icon"
-          src={bannerIcon5}
-        />
-        <Image
-          sx={styles.bannerIcon6}
-          className="bannerIcon"
-          alt="banner icon"
-          src={bannerIcon6}
-        />
-        <Image
-          sx={styles.bannerIcon7}
-          className="bannerIcon"
-          alt="banner icon"
-          src={bannerIcon7}
-        />
+          <Image
+            sx={styles.bannerIcon5}
+            className="bannerIcon"
+            alt="banner icon"
+            src={bannerIcon5}
+          />
+          <Image
+            sx={styles.bannerIcon6}
+            className="bannerIcon"
+            alt="banner icon"
+            src={bannerIcon6}
+          />
+          <Image
+            sx={styles.bannerIcon7}
+            className="bannerIcon"
+            alt="banner icon"
+            src={bannerIcon7}
+          />
 
 
-        <Image sx={styles.logo} src={gravatarImage} alt="Shayne Fawcett gravatar image" />
+          <Image sx={styles.logo} src={gravatarImage} alt="Shayne Fawcett gravatar image" />
 
-        <motion.div 
-        initial="hidden" 
-        animate="visible"
-        transition={{
-          delay: 0.6, type: 'tween'
-        }} 
-        variants={{
-            hidden: {
-              scale: .8,
-              opacity: 0
-            },
-            visible: {
-              scale: 1,
-              opacity: 1,
-              transition: {
-                delay: 1
-              }
-            },
-          }}
-        >
-          <Text as="p">Hi, I'm Shayne</Text>
-          <Heading as="h1">I can turn your
-            <span>
-              <Typed
-                strings={["thoughts", "designs", "prototypes", "data", "ideas", "mockups", "content", "pitches"]}
-                typeSpeed={200}
-                backSpeed={50}
-                backDelay={1000}
-                loop
-                smartBackspace
-                cursorChar={'_'}
-                className='typed'
-              /><br />
-            </span>
-            into user-centric, interactive web experiences.
-          </Heading>
+          <motion.div 
+          initial="hidden" 
+          animate="visible"
+          transition={{
+            delay: 0.6, type: 'tween'
+          }} 
+          variants={{
+              hidden: {
+                scale: .8,
+                opacity: 0
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 1
+                }
+              },
+            }}
+          >
+            <Text as="p">Hi, I'm Shayne ✌🏿</Text>
+            <Heading as="h1">I'll turn your
+              <span>
+                <Typed
+                  strings={["thoughts", "designs", "prototypes", "data", "ideas", "mockups", "content", "pitches"]}
+                  typeSpeed={200}
+                  backSpeed={50}
+                  backDelay={1000}
+                  loop
+                  smartBackspace
+                  cursorChar={'_'}
+                  className='typed'
+                /><br />
+              </span>
+              into user-centric, interactive web experiences.
+            </Heading>
+            
+
+          </motion.div>
+
           
-          <Heading as="h2">
-            With my background in web development, UX, and marketing, I can unlock your idea's potential and bring your brand to life with a stunning online presence. 
-          </Heading>
-        </motion.div>
+          {
+            typeof mobile !== 'undefined' ? (
+              mobile ? (
+                <motion.div 
+                  initial="hidden" 
+                  animate="visible"
+                  transition={{
+                    delay: 0.8, type: 'tween'
+                  }} 
+                  variants={{
+                      hidden: {
+                        scale: .8,
+                        opacity: 0
+                      },
+                      visible: {
+                        scale: 1,
+                        opacity: 1,
+                        transition: {
+                          delay: 1
+                        }
+                      },
+                    }}
+                  >
+                    <Heading as="h2">
+                      With my background in web development, UX, and marketing, I can unlock your idea's potential and bring your brand to life with a stunning online presence. 
+                    </Heading>
 
+                  </motion.div>
+              ) : (
+                <motion.div
+                  initial={{ y: 250 }}
+                  animate={{ y: -23 }}
+                  transition={{ delay:0.2, type: 'spring', stiffness: 500 }}
+                >
+                  
+                  <Container sx={styles.macbook} className='macbook'>
+                    <Box sx={styles.screen}>
+                      <Box 
+                        sx={styles.viewport} 
+                        style={{ backgroundImage: `url(${macbookPage})`}}
+                        className='viewport'
+                      >
         
-        
-        <motion.div
-          initial={{ y: 250 }}
-          animate={{ y: -23 }}
-          transition={{ delay:0.2, type: 'spring', stiffness: 500 }}
-        >
+                      </Box>
+                    </Box>
+                    <Box sx={styles.base}></Box>
+                    <Box sx={styles.notch}></Box>
+                  </Container>
+                  
+                </motion.div>
+              )
+            ) : null
+          }
           
-          <Container sx={styles.macbook} className='macbook'>
-            <Box sx={styles.screen}>
-              <Box 
-                sx={styles.viewport} 
-                style={{ backgroundImage: `url(${macbookPage})`}}
-                className='viewport'
-              >
 
-              </Box>
-            </Box>
-            <Box sx={styles.base}></Box>
-            <Box sx={styles.notch}></Box>
-          </Container>
-          
-        </motion.div>
 
-      </Container>
+        </Container>
+      </Box>
     </Box>
   );
 };
@@ -188,6 +236,7 @@ const styles = {
     overflow: 'hidden',
     backgroundColor: '#232946',
     textAlign: 'center',
+
     pt: ['110px', null, null, null, '130px'],
     h1: {
       fontSize: ['28px', null, null, '32px', '38px', '48px', '64px'],
@@ -195,7 +244,7 @@ const styles = {
       color: '#fffffe',
       fontWeight: 800,
       width: '100%',
-      maxWidth: ['100%', null, null, '60%', '500px', '640px', '851px'],
+      maxWidth: ['100%', null, null, '60%', '600px', '640px', '851px'],
       mx: 'auto',
       mt: '30px',
       mb: ['40px', null, null, '65px'],
@@ -216,7 +265,7 @@ const styles = {
         WebkitTextFillColor: 'transparent',
       },
 
-      '@media(max-width: 600px)': {
+      '@media(max-width: 699px)': {
         textAlign: 'left',
       },
     },
@@ -232,7 +281,7 @@ const styles = {
       mt: '30px',
       mb: ['40px', null, null, '65px'],
 
-      '@media(max-width: 600px)': {
+      '@media(max-width: 699px)': {
         display: 'block',
         textAlign: 'left',
         width: '100%',
@@ -243,14 +292,29 @@ const styles = {
       fontSize: '1.25rem',
     },
   },
+
+  bg: {
+    width: '100%',
+    height: '100%',
+
+      '@media(max-width: 991px)': {
+
+        backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' version='1.1' xmlns:xlink='http://www.w3.org/1999/xlink' xmlns:svgjs='http://svgjs.dev/svgjs' viewBox='0 0 150 150' width='150' height='150' opacity='1'%3E%3Cdefs%3E%3Cfilter id='nnnoise-filter' x='-20%25' y='-20%25' width='140%25' height='140%25' filterUnits='objectBoundingBox' primitiveUnits='userSpaceOnUse' color-interpolation-filters='linearRGB'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.184' numOctaves='4' seed='15' stitchTiles='stitch' x='0%25' y='0%25' width='100%25' height='100%25' result='turbulence'%3E%3C/feTurbulence%3E%3CfeSpecularLighting surfaceScale='31' specularConstant='0.7' specularExponent='20' lighting-color='%23ff8e3c' x='0%25' y='0%25' width='100%25' height='100%25' in='turbulence' result='specularLighting'%3E %3CfeDistantLight azimuth='3' elevation='65'%3E%3C/feDistantLight%3E %3C/feSpecularLighting%3E%3C/filter%3E%3C/defs%3E%3Crect width='700' height='700' fill='%23232946'%3E%3C/rect%3E%3Crect width='700' height='700' fill='%23ff8e3c' filter='url(%23nnnoise-filter)'%3E%3C/rect%3E%3C/svg%3E")`,
+      },
+
+  },
   
   logo: {
-    display: 'block',
+    display: 'none',
     borderRadius: '50%',
     mx: 'auto',
     boxShadow: '0px 15px 35px rgba(65, 104, 139, 0.12)',
     transform: 'translatey(0px)',
     animation: `${bannerAnim4} 4s ease-in-out infinite`,
+
+    '@media(max-width: 699px)': {
+      display: 'block',
+    },
   },
   bannerImage: {
     display: 'block',
@@ -260,6 +324,7 @@ const styles = {
   },
   container: {
     position: 'relative',
+
     '.bannerIcon': {
       position: 'absolute',
       display: ['none', null, null, null, 'block'],
@@ -302,7 +367,7 @@ const styles = {
   macbook: {
     display: 'block',
 
-    '@media(max-width: 600px)': {
+    '@media(max-width: 699px)': {
       display: 'none',
     },
   },
